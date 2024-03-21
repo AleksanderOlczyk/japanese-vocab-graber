d462a29f743e176857fc6c7fa4c9646b27fb2245
Aleksander Olczyk <s27686@pjwstk.edu.pl>
2024-03-21 15:27:23 +0100

    Code clean up

--- Anki/Grab_words_from_excel/read from excel.py
+++ Anki/Grab_words_from_excel/read from excel.py
@@ -7,8 +7,8 @@ def wczytaj_komorki_z_excel(plik, arkusz, poczatkowy_wiersz, pierwsza_komorka, d
         df = pd.read_excel(plik, sheet_name=arkusz, skiprows=poczatkowy_wiersz - 1)
         komorki_B_C = df[[pierwsza_komorka, druga_komorka]]
         komorki_B_C = komorki_B_C.dropna()
-        komorki_B_C['ano'] = komorki_B_C['ano'].str.strip()
-        komorki_B_C['"Zwracamy na siebie uwagę odbiorcy"'] = komorki_B_C['"Zwracamy na siebie uwagę odbiorcy"'].str.strip()
+        komorki_B_C[druga_komorka] = komorki_B_C[druga_komorka].str.strip()
+        komorki_B_C[pierwsza_komorka] = komorki_B_C[pierwsza_komorka].str.strip()
         return komorki_B_C
     except Exception as e:
         print("Wystąpił błąd podczas wczytywania danych z pliku Excel:", e)
@@ -17,17 +17,18 @@ def wczytaj_komorki_z_excel(plik, arkusz, poczatkowy_wiersz, pierwsza_komorka, d
 
 
 plik_excel = 'Słówka　Genki.xlsx'
-nazwa_arkusza = '１課'
-poczatkowy_wiersz = 2
-pierwsza_komorka = '"Zwracamy na siebie uwagę odbiorcy"'
-druga_komorka = 'ano'
+nazwa_arkusza = '２課'
+poczatkowy_wiersz = 3
+pierwsza_komorka = 'to (w moim zasięgu)'
+druga_komorka = 'kore'
+page = 2
 
 komorki_B_C = wczytaj_komorki_z_excel(plik_excel, nazwa_arkusza, poczatkowy_wiersz, pierwsza_komorka, druga_komorka)
 
 if komorki_B_C is not None:
     print("Wczytane komórki z kolumn B i C:")
     print(komorki_B_C.to_string(index=False, justify='left'))
-    nazwa_pliku_txt = 'Genki_1.txt'
+    nazwa_pliku_txt = f'Genki_{page}.txt'
     komorki_B_C.to_csv(nazwa_pliku_txt, index=False, header=None, sep='\t', encoding='utf-8')
 
     print("Dane zostały zapisane do pliku:", nazwa_pliku_txt)
